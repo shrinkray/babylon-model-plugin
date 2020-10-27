edd2f18b87f237e3bfc7dedbd51a785638d1c0aa
Greg <hello@gregmiller.io>
2020-10-27 15:25:40 -0400

    Initial commit

---
+++ babylon-model-plugin.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name:       Babylon Model Plugin
+ * Plugin URI:        https://github.com/shrinkray/babylon-model-plugin
+ * Description:       Reusable tooling for Soss Hinge Models.
+ * Version:           1.0.0
+ * Requires at least: 5.3
+ * Requires PHP:      7.4
+ * Author:            Greg Miller
+ * Author URI:        https://gregmiller.io
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       babylon-model-plugin
+ * Domain Path:       /languages
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // disable direct access.
+}
